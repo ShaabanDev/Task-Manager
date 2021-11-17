580b23894993539b9ee022c1f88b91cb73ea772d
Mohamed <qotozer@gmail.com>
2021-11-17 15:25:09 +0200

    create db and conecting app to it using mongodb module.

---
+++ mongodb.js
@@ -0,0 +1,16 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+
+const dbName = "task-manager";
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true },(error,client)=>{
+    if(error){
+        console.log(error);
+        return  console.log('unable to connect to database');
+    }
+
+    console.log('connected correctly');
+});
