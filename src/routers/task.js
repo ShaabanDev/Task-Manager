a205a1aeb4c6ed957e0fa192ad706a10c879baf1
Mohamed <qotozer@gmail.com>
2021-11-21 20:59:55 +0200

    moving the routers into new separated files

---
+++ src/routers/task.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/task");
+
+// ------------POST------------
+// post new task on the server side to then it again the client side
+router.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.send(task);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+// ------------GET------------
+// getting the task by it id
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+// getting all the tasks
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// ------------PATCH (UPDATE)------------
+// update task by it is's id
+router.patch("/tasks/:id", async (req, res) => {
+  const availableUpdates = ["description", "completed"];
+  const requestKeys = Object.keys(req.body);
+  const isValid = requestKeys.every((update) =>
+    availableUpdates.includes(update)
+  );
+  if (!isValid) {
+    return res.status(404).send({ error: "invalid updates" });
+  }
+  try {
+    const _id = req.params.id;
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// ------------DELETE------------
+
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
---
+++ src/routers/user.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const User = require("../models/user");
+const router = express.Router();
+
+// getting all the users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+// getting the user using his id
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+// ------------POST------------
+
+// post new user to the server side then send it again to the client side
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+// ------------PATCH (UPDATE)------------
+
+// update user by it is's id
+router.patch("/users/:id", async (req, res) => {
+  const availableUpdates = ["name", "age", "password", "email"];
+  const requestKeys = Object.keys(req.body);
+  const isValid = requestKeys.every((update) =>
+    availableUpdates.includes(update)
+  );
+  if (!isValid) {
+    return res.status(404).send({ error: "invalid updates" });
+  }
+  const _id = req.params.id;
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// ------------DELETE------------
+
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
