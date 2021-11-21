919beb8ae8e393aece7eaaa04ea56b5aad5b67d2
Mohamed <qotozer@gmail.com>
2021-11-21 18:03:02 +0200

    using AsyncAwait patten instead of then pattern

--- src/index.js
+++ src/index.js
@@ -12,81 +12,80 @@ app.use(express.json());
 // ------------TASKS-----------
 
 // getting all the tasks
-app.get('/tasks',(req,res)=>{
-  Task.find().then((tasks) => {
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
     res.send(tasks);
-  }).catch((err) => {
-    res.status(400).send();
-  });
-})
-// getting the task by it id 
-app.get('/tasks/:id',(req,res)=>{
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+// getting the task by it id
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
-  Task.findById(_id).then((task) => {
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
     res.send(task);
-  }).catch((err) => {
-    res.status(500).send()
-  });
-})
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 
 // ------------USERS-----------
 
 // getting all the users
-app.get("/users", (req, res) => {
-  User.find()
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send();
+  }
 });
 
-
 // getting the user using his id
-app.get("/users/:id", (req, res) => {
-    const _id = req.params.id;
-    User.findById(_id).then((user) => {
-      res.send(user);
-    }).catch((err) => {
-      res.status(500).send();
-    });
-  });
-
+app.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 
 // ------------POST------------
 
 // ------------USERS-----------
 
 // post new user to the server side then send it again to the client side
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  try {
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
-
-
-// ------------USERS-----------
+// ------------TASKS-----------
 
 // post new task on the server side to then it again the client side
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then((task) => {
-      res.send(task);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  try {
+    await task.save();
+    res.send(task);
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
-
 
 // start listening to the server
 app.listen(port, () => {
