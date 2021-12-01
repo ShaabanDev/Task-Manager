dc7931e2e9ddd67ea45cdb815989d726f757d24f
Mohamed <qotozer@gmail.com>
2021-12-01 21:45:53 +0200

    sorting data

--- src/routers/task.js
+++ src/routers/task.js
@@ -37,18 +37,24 @@ router.get("/tasks/:id", auth, async (req, res) => {
 });
 // getting all the tasks
 router.get("/tasks", auth, async (req, res) => {
-  const match ={}
-  if(req.query.completed){
-    match.completed = req.query.completed
+  const match = {};
+  const sort = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed;
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
   try {
     await req.user.populate({
-      path:"tasks",
+      path: "tasks",
       match,
-      options:{
-        limit:parseInt(req.query.limit),
-        skip:parseInt(req.query.skip)
-      }
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
     });
     res.send(req.user.tasks);
   } catch (error) {
