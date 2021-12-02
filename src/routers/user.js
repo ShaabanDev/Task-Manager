0dbbfe9c8b9ed5c8df7a808e33c21a7f2031f71b
Mohamed <qotozer@gmail.com>
2021-12-02 16:46:14 +0200

    adding get route to get the avatar image of an user

--- src/routers/user.js
+++ src/routers/user.js
@@ -100,17 +100,36 @@ const upload = multer({
   },
 });
 
-router.post("/users/me/avatar",auth , upload.single("avatar"), async (req, res) => {
-  req.user.avatar =  req.file.buffer;
-  await req.user.save();
-  res.send();
-},(error,req,res,next)=>{
-  res.status(400).send({error:error.message})
-});
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
-router.delete('/users/me/avatar',auth,async (req,res)=>{
+router.delete("/users/me/avatar", auth, async (req, res) => {
   req.user.avatar = undefined;
   await req.user.save();
   res.send();
-})
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if(!user || !user.avatar){
+      throw new Error();
+    }
+    res.set("Content-Type", "image/jpg");
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(404).send();
+  }
+});
 module.exports = router;
